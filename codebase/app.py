9d447ce4556f7fd41bca1fa1079cc53287c904ed
Sarath Sajan <sarathsajan1998@gmail.com>
2022-04-21 09:59:48 +0530

    get real time data

---
+++ codebase/app.py
@@ -0,0 +1,22 @@
+import requests
+import datetime
+
+# List of Coin pairs to check
+symbol_list = ['ethinr', 'adainr', 'linkinr', 'uniinr', 'algoinr', 'nearinr', 'lunainr', 'manainr', 'xlminr', 'dotinr', 'btcinr']
+
+# REST API URL for WazirX Exchange
+rest_api_url = 'https://api.wazirx.com/sapi/v1/tickers/24hr'
+
+# Get the current price data from WazirX Exchange, and convert to JSON
+current_market_status_raw = requests.get(rest_api_url)
+current_market_status_json = current_market_status_raw.json()
+
+# Get current UTC time in ISO 8601 format, add offset to match Indian Standard Time, and format the string to 'Y_m_d_H_M_S'
+print(datetime.datetime.now(tz=datetime.timezone(offset=datetime.timedelta(hours=5, minutes=30))).strftime("%Y_%m_%d_%H_%M_%S"))
+
+# Loop through each coin pair in the list and
+# print the current price of coin pair present
+# in the symbol list
+for pair in current_market_status_json:
+  if pair['symbol'] in symbol_list:
+    print(pair['symbol'], ' \t-', pair['lastPrice'])
