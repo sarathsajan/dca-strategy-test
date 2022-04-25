718883dfdd60fe315eb4d4d50379c78e8a174552
Sarath Sajan <sarathsajan1998@gmail.com>
2022-04-25 21:15:36 +0530

    more inclusive file generator

--- codebase/check_file_generator.py
+++ codebase/check_file_generator.py
@@ -10,6 +10,9 @@ with open("symbol_list.csv", 'r', newline='', encoding='utf-8') as symbol_list_f
 # check if nescessary files exist in price_data folder
 # if not, create them
 for symbol in symbol_list:
+    price_data_folder = pathlib.Path(f"price_data")
+    if not price_data_folder.is_dir():
+        price_data_folder.mkdir()
     price_data_file = pathlib.Path(f"price_data/{symbol}.csv")
     if not price_data_file.is_file():
         with open(f"price_data/{symbol}.csv", 'w+', newline='', encoding='utf-8') as price_data_file:
@@ -18,9 +21,12 @@ for symbol in symbol_list:
 # check if nescessary files and sub folders exist in episodes/symbol folder
 # if not, create them
 for symbol in symbol_list:
-    episode_folder = pathlib.Path(f"episodes/{symbol}")
+    episode_folder = pathlib.Path(f"episodes")
     if not episode_folder.is_dir():
         episode_folder.mkdir()
+    episode_symbol_folder = pathlib.Path(f"episodes/{symbol}")
+    if not episode_symbol_folder.is_dir():
+        episode_symbol_folder.mkdir()
     episode_file = pathlib.Path(f"episodes/{symbol}/{symbol}_episode_current.csv")
     if not episode_file.is_file():
         with open(f"episodes/{symbol}/{symbol}_episode_current.csv",'w+', newline='', encoding='utf-8') as episode_file:
