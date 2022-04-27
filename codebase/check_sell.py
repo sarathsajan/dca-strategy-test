bf40f09fdef03ade16520e3270600b303c60fda1
Sarath Sajan <sarathsajan1998@gmail.com>
2022-04-28 00:33:34 +0530

    added dynamic profit calculator

--- codebase/check_sell.py
+++ codebase/check_sell.py
@@ -22,23 +22,27 @@ for symbol in symbol_list:
     if len(episode_buy_details_list) > 0:
         # get the total amount of capital A, spend in the current episode
         A = sum([float(buy_details[2]) for buy_details in episode_buy_details_list])
-        print(A)
+        print('total amount of capital - ', A)
 
         # get the total number of items bought N, in the current episode
         N = sum([float(buy_details[4]) for buy_details in episode_buy_details_list])
-        print(N)
+        print('total number of items bought - ', N)
 
-        # profit percentage indicated as fraction 3% meaning 103% meaning 1.03
-        P = 1.03
+        # Profit percentage will dynamically increase as the no. of BUY positions increase.
+        # The base profit percentage is 1%
+        # Each BUY position will increase profit percentage by 0.2%
+        base_p = 1.01
+        P = base_p + (0.002 * len(episode_buy_details_list))
+        print('profit percentage - ', P)
 
-        # after getting P percent profit, the episode portfolio will
-        # reach Z amount of capital
+        # When P percent profit is achieved, the episode portfolio will
+        # have reached Z amount of capital
         Z = A * P
-        print(Z)
+        print('capital with profit - ', Z)
 
-        # price to SELL S, to make P percent profit
+        # The price to at which to sell S, in order to make P percent profit is given by
         S = Z / N
-        print(S)
+        print('price to sell at - ', S)
 
         # opening the CSV file in read mode and reading all the hourly data and
         # storing it as list. All this to just get the current price
@@ -50,7 +54,7 @@ for symbol in symbol_list:
             sell_details = []
             with open(f"episodes/{symbol}/{symbol}_episode_current.csv", 'a', newline='', encoding='utf-8') as episode_file:
                 # sell_details = [SELL, timestamp, capital gained by selling, current price, no. of items sold]
-                sell_details.append(['SELL', datetime.datetime.now(tz=datetime.timezone(offset=datetime.timedelta(hours=5, minutes=30))).strftime("%Y%m%d%H%M%S"), Z, current_price, Z/current_price])
+                sell_details.append(['SELL', datetime.datetime.now(tz=datetime.timezone(offset=datetime.timedelta(hours=5, minutes=30))).strftime("%Y%m%d%H%M%S"), Z, current_price, N])
                 csv.writer(episode_file).writerows(sell_details)
             
             # since the current episode is now finished, copy the current episode csv file to another csv file with timestamp
