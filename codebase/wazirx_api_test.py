de8bbb30fbd50ccfdec7fcb95d532da8272de963
Sarath Sajan <sarathsajan1998@gmail.com>
2022-11-05 00:38:45 +0530

    securely stored the API and SECRET keys

--- codebase/wazirx_api_test.py
+++ codebase/wazirx_api_test.py
@@ -1,15 +1,14 @@
 import ccxt
+from env_vars import ENV_VARS
 
-# provide an api_key and secret_key as keyword arguments
-secret_key = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
-API_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxx'
+ENVIRONMENT_VARIABLES = ENV_VARS()
 
 # connect wazirx
 wazirx = ccxt.wazirx()
-wazirx.apiKey = API_key
-wazirx.secret = secret_key
+wazirx.apiKey = ENVIRONMENT_VARIABLES.WAZIRX_API_KEY
+wazirx.secret = ENVIRONMENT_VARIABLES.WAZIRX_SECRET_KEY
 
 amount = 3
-price = 17.44
+price = 30
 
-wazirx.create_order('WRX/INR', 'limit', 'sell', amount, price)
+wazirx.create_order('BTC/INR', 'limit', 'buy', amount, price)
