1e75ea7cb2ce7ab866fc380dff7fd199fb4b93f0
Sarath Sajan <sarathsajan1998@gmail.com>
2022-11-05 19:32:08 +0530

    wayscript bash doesnt recognise terminal clear command

--- codebase/app.py
+++ codebase/app.py
@@ -7,41 +7,62 @@ import time
 import datetime
 import os
 
-os.system('cls')
+try:
+    os.system('cls')
+except:
+    pass
 job_start_dttm = datetime.datetime.now(tz=datetime.timezone(offset=datetime.timedelta(hours=5, minutes=30))).strftime("%Y-%m-%d %H:%M:%S")
 print('job_start_dttm\t-->\t', job_start_dttm)
 time.sleep(5)
 
-os.system('cls')
+try:
+    os.system('cls')
+except:
+    pass
 print('step 1/5 checking and validating files...')
 time.sleep(5)
 file_generator()
 time.sleep(5)
 
-os.system('cls')
+try:
+    os.system('cls')
+except:
+    pass
 print('step 2/5 getting current data (hourly)')
 time.sleep(5)
 hourly_price()
 time.sleep(10)
 
-os.system('cls')
+try:
+    os.system('cls')
+except:
+    pass
 print('step 3/5 running conditional checks and creating "BUY limit orders" if conditions are satisfied')
 time.sleep(5)
 buy()
 time.sleep(5)
 
-os.system('cls')
+try:
+    os.system('cls')
+except:
+    pass
 print('step 4/5 running conditional checks and creating "SELL limit orders" if conditions are satisfied')
 time.sleep(5)
 sell()
 time.sleep(5)
 
-os.system('cls')
+try:
+    os.system('cls')
+except:
+    pass
 print('step 5/5 generating stats...')
 current_statistics()
 time.sleep(5)
 
-os.system('cls')
+try:
+    os.system('cls')
+except:
+    pass
 job_end_dttm = datetime.datetime.now(tz=datetime.timezone(offset=datetime.timedelta(hours=5, minutes=30))).strftime("%Y-%m-%d %H:%M:%S")
 print('job_start_dttm\t-->\t', job_start_dttm)
 print('job_end_dttm\t-->\t', job_end_dttm)
